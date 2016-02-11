4c8533908abfebbe325d1bafcc1209d32b8e4ab3
Matthieu Laqua <mlmatthieul@gmail.com>
2016-02-11 20:26:27 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# ----------------------
+# Small Pong game
+# @author Matthieu Laqua
+# ----------------------
+
+# main function
+def main():
+    pass
+
+# entrypoint
+if __name__ == "__main__":
+    main()
+
