bd42c77d37785dd97259252e72fe4f495ada46b6
Matthieu Laqua <mlmatthieul@gmail.com>
2016-02-11 20:37:16 +0100

    Hello Termbox!

--- main.py
+++ main.py
@@ -4,9 +4,17 @@
 # @author Matthieu Laqua
 # ----------------------
 
+import time, termbox
+
 # main function
 def main():
-    pass
+    tb = termbox.Termbox()
+    somestr = "Hello Termbox!"
+    for i in range(len(somestr)):
+        tb.change_cell(i,1, ord(somestr[i]), 0, 0)
+    tb.present()
+    time.sleep(3)
+    tb.close()
 
 # entrypoint
 if __name__ == "__main__":
