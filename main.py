f0ba8aaa2420b8908f27d304cf9a8818930c2bf7
Matthieu Laqua <mlmatthieul@gmail.com>
2016-02-11 20:51:49 +0100

    Added render_border function

--- main.py
+++ main.py
@@ -11,10 +11,24 @@ def main():
     tb = termbox.Termbox()
     somestr = "Hello Termbox!"
     for i in range(len(somestr)):
-        tb.change_cell(i,1, ord(somestr[i]), 0, 0)
+        tb.change_cell(i+1,1, ord(somestr[i]), 0, 0)
+
+    render_border(tb)
+
     tb.present()
     time.sleep(3)
     tb.close()
+
+# render a basic border
+def render_border(p_tb, p_char='#'):
+    p_char = ord(p_char)
+    width, height = p_tb.width(), p_tb.height()
+    for x in range(width):
+        p_tb.change_cell(x, 0, p_char, 0, 0)
+        p_tb.change_cell(x, height-1, p_char, 0, 0)
+    for y in range(height):
+        p_tb.change_cell(0, y, p_char, 0, 0)
+        p_tb.change_cell(width-1, y, p_char, 0, 0)
 
 # entrypoint
 if __name__ == "__main__":
